5021338744e3c0ff0bda9a2942d25bf412687791
Demetrice Paulo-Tuttle <dpt@Demetrices-MBP.attlocal.net>
2022-09-03 23:50:03 -0700

    Latest PyPoll Data

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,15 @@ outputfilepath = os.path.join("Analysis", "election_analysis.txt")
 
 #Open the election results and read the file.
 
+#Initialize vote count to 0
+
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = 0
+winning_count = 0
+winning_percentage = 0
+
 with open(csvfilepath) as election_data:
 
 #To do: perform analysis
@@ -31,5 +40,37 @@ with open(csvfilepath) as election_data:
     
     headers = next(file_reader)
 
-    print(headers)
+    for i in file_reader:
+        total_votes +=1
+        candidate_name = i[2]
+        if candidate_name not in candidate_options:
+            #Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+            #Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+        
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+        
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
